ef92714b50d4ec131dd916b38c68d8651c7b3dfa
divyakodenikiri <divyapadpu99@gmail.com>
2022-12-14 11:02:06 +0530

    new text added

---
+++ example1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  
+<head>
+    <style>
+        h1 {
+            color: green;
+        }
+    </style>
+</head>
+  
+<body>
+    <center>
+        <h1>Welcome To GFG</h1>
+  
+        <h2>
+            <?php 
+            echo "This is PHP code inside html<br>";
+            echo "My new project"
+        
+            ?>
+        </h2>
+    </center>
+</body>
+  
+</html>

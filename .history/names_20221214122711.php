88da445033589d093004e6b7df63af98e8beb32c
divyakodenikiri <divyapadpu99@gmail.com>
2022-12-14 12:28:24 +0530

    multi php tags

---
+++ .history/names_20221214122711.php
@@ -0,0 +1,21 @@
+<?php
+$names = ['kavya','bavye','divya','shanu'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="">
+    <h1>$names</h1>
+    <ul>
+        <?php foreach($names as $name){ ?>
+       <li><?= $name ?></li>
+      <?php  } ?>
+    </ul>  
+
+</body>
+</html>
